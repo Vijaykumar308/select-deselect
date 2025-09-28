093b266dca272b7c2440649a04438c4434cf225f
Vijay Kumar <308vijaykumar@gmail.com>
2025-09-28 13:57:05 +0530

    logic implementing

--- src/app/tic-tac-toe/components/Board.jsx
+++ src/app/tic-tac-toe/components/Board.jsx
@@ -1,24 +1,25 @@
 'use client';
 
+import { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
     return<>
         <div className="board-container bg-green-200 w-60 border-2 rounded m-10">
             <div className="board-row flex justify-evenl"> 
-                <Square />
-                <Square />
-                <Square />
+                <Square index={0}/>
+                <Square index={1}/>
+                <Square index={2}/>
             </div>
              <div className="board-row flex justify-evenly"> 
-                <Square />
-                <Square />
-                <Square />
+                <Square index={3}/>
+                <Square index={4}/>
+                <Square index={5}/>
             </div>
              <div className="board-row flex justify-evenly"> 
-                <Square />
-                <Square />
-                <Square />
+                <Square index={6}/>
+                <Square index={7}/>
+                <Square index={8}/>
             </div>
         </div>
     </>
--- src/app/tic-tac-toe/components/Square.jsx
+++ src/app/tic-tac-toe/components/Square.jsx
@@ -1,9 +1,24 @@
 'use client';
 
-const Square = () => {
+import { useState } from "react";
+
+const Square = ({index}) => {
+    const [state, setState] = useState(Array(9).fill(null));
+    const [isXTurn, setIsXTurn] = useState(true);
+
+    
+    const handleClicked = (id) => {
+        const symbole = "X";
+        console.log('clicked in func: '+id);
+        const copyState = [...state];
+        copyState[id] = symbole;
+        console.log(copyState);
+        setState([...copyState]);
+    }
+
     return <>
-        <div className="square border-2 w-80 h-20 flex justify-center items-center">
-           X
+        <div className="square border-2 w-80 h-20 flex justify-center items-center" onClick={() => handleClicked(index)}>
+          {state[index]}
         </div>
     </>
 }
